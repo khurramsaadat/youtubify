8784b10a3a00771525c398d9404d9c520c12b64e
Khurram Saadat <khurram.saadat@yahoo.com>
2025-11-21 15:35:05 +0400

    Landing page rendered

---
+++ src/unAuth/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1 className="landing-title">Hello Landing Page</h1>
+        <p className="landing-subtitle">Welcome to YouTubify</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
+
